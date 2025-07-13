dfdb9644a44510c94587617362a358f3498855f9
saakshidas <sakshidas83498@gmail.com>
2025-07-13 20:52:09 +0530

    removed unwanted import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
-import About from "./components/about";
-import Services from "./components/services";
-import Portfolio from "./components/portfolio";
-import Testimonials from "./components/testimonial";
-import Contact from "./components/contact";
-import Footer from "./components/footer";
 
 export default function Home() {
   return (
